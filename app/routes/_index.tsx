ec1be94fa9874af39c64eefd616b29a6b998ac4b
ndyabagye <ndyabagyehenrytusi@gmail.com>
2024-02-28 12:46:30 +0300

    First commit on  the remix tutorial

---
+++ app/routes/_index.tsx
@@ -0,0 +1,9 @@
+export default function Index() {
+  return (
+    <p id="index-page">
+      This is a demo for remix
+      <br />
+      Check out <a href="https://remix.run">the docs at remix.run</a>.
+    </p>
+  );
+}
